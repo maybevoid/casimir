b20e78246e50c98f72466001e7d68ab238991744
Soares Chen <soares.chen@gmail.com>
2020-06-09 22:16:02 +0200

    Simplify Effects. Now compiles

--- casimir/src/base/Casimir/Base.hs
+++ casimir/src/base/Casimir/Base.hs
@@ -5,7 +5,6 @@
 -}
 module Casimir.Base
   ( Effect (..)
-  , NormalizedEffects (..)
   , EffFunctor (..)
 
   , ImplicitOps
--- casimir/src/base/Casimir/Base/Effect.hs
+++ casimir/src/base/Casimir/Base/Effect.hs
@@ -3,8 +3,6 @@
 module Casimir.Base.Effect
   ( Effect (..)
   , Effects (..)
-  , NormalizedEffects (..)
-  , Operations
   , Singleton
   , NoEff
   , Union
@@ -21,67 +19,34 @@ data Singleton eff
 data Cons eff1 eff2
 data Union eff1 eff2
 
-class NormalizedEffects eff where
-  type family Operations' eff
-    = (ops :: (Type -> Type) -> Type) | ops -> eff
-
-class
-  ( NormalizedEffects (NormalizeEffect xs) )
-  => Effects (xs :: k) where
-    type family NormalizeEffect xs = eff | eff -> xs
+class Effects (effs :: k) where
+    type family Operations effs
+      = (ops :: (Type -> Type) -> Type) | ops -> effs
 
 class Effect eff where
   type family Operation eff
     = (ops :: (Type -> Type) -> Type) | ops -> eff
 
-type Operations effs = Operations' (NormalizeEffect effs)
-
 instance ( Effect eff )
   => Effects (Singleton eff) where
-    type NormalizeEffect (Singleton eff) = Singleton eff
-
-instance
-  ( Effect eff )
-  => NormalizedEffects (Singleton eff) where
-    type Operations' (Singleton eff) =
+    type Operations (Singleton eff) =
       Param.Singleton (Operation eff)
 
-
 instance Effects ('[] :: [Type]) where
-  type NormalizeEffect '[] = NoEff
-
-instance NormalizedEffects NoEff where
-  type Operations' NoEff = Param.Nil
-
-
-instance
-  (NormalizedEffects ops1, NormalizedEffects ops2)
-  => NormalizedEffects (Union ops1 ops2)
-  where
-    type Operations' (Union ops1 ops2) =
-      Param.Union (Operations' ops1) (Operations' ops2)
+  type Operations '[] = Param.Nil
 
 instance
   ( Effects effs1
   , Effects effs2
   )
   => Effects (Union effs1 effs2) where
-    type NormalizeEffect (Union effs1 effs2) =
-      Union (NormalizeEffect effs1) (NormalizeEffect effs2)
-
-
-instance
-  ( Effect x
-  , Effects xs
-  )
-  => Effects (x:xs) where
-    type NormalizeEffect (x:xs) =
-      Cons x (NormalizeEffect xs)
+    type Operations (Union effs1 effs2) =
+      Param.Union (Operations effs1) (Operations effs2)
 
 instance
   ( Effect eff
-  , NormalizedEffects effs
+  , Effects effs
   )
-  => NormalizedEffects (Cons eff effs) where
-    type Operations' (Cons eff effs) =
-      Param.Cons (Operation eff) (Operations' effs)
+  => Effects (eff:effs) where
+    type Operations (eff:effs) =
+      Param.Cons (Operation eff) (Operations effs)
